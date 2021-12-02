a2ada0ea650bc553b31c4cab7724b762ab4d2bd4
smsutherland <sagan@farthinghalearms.com>
2021-12-02 00:24:10 -0500

    cargo fmt

--- src/day_2.rs
+++ src/day_2.rs
@@ -1,31 +1,30 @@
 pub fn run(lines: &[String]) -> (u32, u32) {
-
     let mut x = 0;
     let mut d = 0;
 
     let mut aim = 0;
     let mut d_2 = 0;
-    for line in lines{
+    for line in lines {
         let mut line_split = line.split(" ");
         let com = line_split.next().unwrap();
         let val: u32 = line_split.next().unwrap().parse().unwrap();
 
-        match com{
+        match com {
             "forward" => {
                 x += val;
-                d_2 += aim*val;
-            },
+                d_2 += aim * val;
+            }
             "down" => {
                 d += val;
                 aim += val;
-            },
+            }
             "up" => {
                 d -= val;
                 aim -= val;
-            },
+            }
             _ => (),
         }
     }
 
-    (x*d, x*d_2)
+    (x * d, x * d_2)
 }
