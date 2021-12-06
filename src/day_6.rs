3334422a77a51ca3a1bbe297a766cdac62751bdb
smsutherland <sagan@farthinghalearms.com>
2021-12-06 00:53:37 -0500

    cargo fmt

--- src/day_6.rs
+++ src/day_6.rs
@@ -1,18 +1,19 @@
 pub fn run(lines: &[String]) -> (u64, u64) {
-
     let line: Vec<u64> = lines[0].split(",").map(|x| x.parse().unwrap()).collect();
-    let mut fish: Vec<u64> = (0..=8).map(|x| line.iter().filter(|y| **y == x).count() as u64).collect();
+    let mut fish: Vec<u64> = (0..=8)
+        .map(|x| line.iter().filter(|y| **y == x).count() as u64)
+        .collect();
 
     let mut part_1 = 0;
 
-    for i in 0..256{
+    for i in 0..256 {
         let mut fish_iter = fish.drain(..);
         let new_fish = fish_iter.next().unwrap();
         fish = fish_iter.collect();
         fish.push(new_fish);
         fish[6] += new_fish;
 
-        if i == 79{
+        if i == 79 {
             part_1 = fish.iter().sum::<u64>();
         }
 
