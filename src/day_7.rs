16f80848e91353032fb44381e47534b660961c1b
smsutherland <sagan@farthinghalearms.com>
2021-12-07 00:43:33 -0500

    one-liner solution

--- src/day_7.rs
+++ src/day_7.rs
@@ -1,24 +1,19 @@
 pub fn run(lines: &[String]) -> (u64, u64) {
+    // ((0..=(*lines[0].split(",").map(|x|x.parse().unwrap()).collect::<Vec<i64>>().iter().max().unwrap())).map(|i|lines[0].split(",").map(|x|x.parse().unwrap()).collect::<Vec<i64>>().iter().map(|x|(i-x).abs()).sum::<i64>()).min().unwrap() as u64,(0..=(*lines[0].split(",").map(|x|x.parse().unwrap()).collect::<Vec<i64>>().iter().max().unwrap())).map(|i|lines[0].split(",").map(|x|x.parse().unwrap()).collect::<Vec<i64>>().iter().map(|x|(i-x).abs()*((i-x).abs()+1)/2).sum::<i64>()).min().unwrap() as u64)
+
     let nums: Vec<i64> = lines[0].split(",").map(|x| x.parse().unwrap()).collect();
-
-    let mut min_fuel = 1000000000000_u64;
-    for i in 0..=(*nums.iter().max().unwrap()) {
-        let fuel: i64 = nums.iter().map(|x| (i - x).abs()).sum();
-        if fuel < min_fuel as i64 {
-            min_fuel = fuel as u64;
-        }
-    }
-
-    let mut min_fuel2 = 1000000000000_u64;
-    for i in 0..=(*nums.iter().max().unwrap()) {
-        let fuel: i64 = nums
-            .iter()
-            .map(|x| (i - x).abs() * ((i - x).abs() + 1) / 2)
-            .sum();
-        if fuel < min_fuel2 as i64 {
-            min_fuel2 = fuel as u64;
-        }
-    }
-
-    (min_fuel, min_fuel2)
+    (
+        (0..=(*nums.iter().max().unwrap()))
+            .map(|i| nums.iter().map(|x| (i - x).abs()).sum::<i64>())
+            .min()
+            .unwrap() as u64,
+        (0..=(*nums.iter().max().unwrap()))
+            .map(|i| {
+                nums.iter()
+                    .map(|x| (i - x).abs() * ((i - x).abs() + 1) / 2)
+                    .sum::<i64>()
+            })
+            .min()
+            .unwrap() as u64,
+    )
 }
