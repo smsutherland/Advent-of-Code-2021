3eeb235d53cc585e9f6735c2ebfabe637a738986
smsutherland <sagan@farthinghalearms.com>
2021-12-18 00:52:29 -0500

    fmt day 17

--- src/day_17.rs
+++ src/day_17.rs
@@ -1,7 +1,9 @@
 use crate::common;
 
-pub fn run(lines: &[String]) -> (u64, u64){
-    let mut area = common::deformat_str("target area: x={}..{}, y={}..{}", &lines[0]).unwrap().into_iter();
+pub fn run(lines: &[String]) -> (u64, u64) {
+    let mut area = common::deformat_str("target area: x={}..{}, y={}..{}", &lines[0])
+        .unwrap()
+        .into_iter();
     let x_min: i32 = area.next().unwrap().parse().unwrap();
     let x_max: i32 = area.next().unwrap().parse().unwrap();
     let y_min: i32 = area.next().unwrap().parse().unwrap();
@@ -11,15 +13,15 @@ pub fn run(lines: &[String]) -> (u64, u64){
 
     let max_v_y = -y_min - 1;
     let min_v_y = -max_v_y - 1;
-    let max_h = (max_v_y*(max_v_y + 1))/2;
+    let max_h = (max_v_y * (max_v_y + 1)) / 2;
 
     let max_v_x = x_max;
     let min_v_x = get_min_v_x(x_min);
 
     let mut total = 0;
-    for v_y in min_v_y..=max_v_y{
-        for v_x in min_v_x..=max_v_x{
-            if reaches_target(v_x, v_y, &x_range, &y_range){
+    for v_y in min_v_y..=max_v_y {
+        for v_x in min_v_x..=max_v_x {
+            if reaches_target(v_x, v_y, &x_range, &y_range) {
                 total += 1;
             }
         }
@@ -28,11 +30,11 @@ pub fn run(lines: &[String]) -> (u64, u64){
     (max_h as u64, total)
 }
 
-fn get_min_v_x(x_min: i32) -> i32{
+fn get_min_v_x(x_min: i32) -> i32 {
     let mut v_x = 0;
     let mut d = 0;
-    loop{
-        if d >= x_min{
+    loop {
+        if d >= x_min {
             return v_x;
         }
 
@@ -41,32 +43,37 @@ fn get_min_v_x(x_min: i32) -> i32{
     }
 }
 
-fn reaches_target(mut v_x: i32, mut v_y: i32, x_range: &std::ops::RangeInclusive<i32>, y_range: &std::ops::RangeInclusive<i32>) -> bool{
+fn reaches_target(
+    mut v_x: i32,
+    mut v_y: i32,
+    x_range: &std::ops::RangeInclusive<i32>,
+    y_range: &std::ops::RangeInclusive<i32>,
+) -> bool {
     let mut x = 0;
     let mut y = 0;
 
-    loop{
+    loop {
         x += v_x;
         y += v_y;
-        if v_x > 0{
+        if v_x > 0 {
             v_x -= 1;
         }
         v_y -= 1;
 
-        if x_range.contains(&x) && y_range.contains(&y){
+        if x_range.contains(&x) && y_range.contains(&y) {
             return true;
         }
-        if x > *x_range.end(){
+        if x > *x_range.end() {
             return false;
         }
-        if y < *y_range.start(){
+        if y < *y_range.start() {
             return false;
         }
     }
 }
 
 #[cfg(test)]
-mod test{
+mod test {
     use super::*;
     #[test]
     fn example_1() {
