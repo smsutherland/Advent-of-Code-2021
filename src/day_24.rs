fff6c181c1c0caddadb43a9a78f0da5ff917f5bd
smsutherland <sagan@farthinghalearms.com>
2021-12-24 15:24:59 -0500

    fmt

--- src/day_24.rs
+++ src/day_24.rs
@@ -46,7 +46,9 @@ fn _simulate_code_raw(input: &str) -> i64 {
 }
 
 fn _simulate_code_readable(input: &str) -> i64 {
-    const Z_DIVISORS: [bool; 14] = [false, false, false, false, true, false, false, true, false, true, true, true, true, true];
+    const Z_DIVISORS: [bool; 14] = [
+        false, false, false, false, true, false, false, true, false, true, true, true, true, true,
+    ];
     const X_ADDERS: [i64; 14] = [14, 13, 15, 13, -2, 10, 13, -15, 11, -9, -9, -7, -4, -6];
     const Y_ADDERS: [i64; 14] = [0, 12, 14, 0, 3, 15, 11, 12, 1, 12, 3, 10, 14, 12];
 
@@ -54,16 +56,14 @@ fn _simulate_code_readable(input: &str) -> i64 {
     let mut z = 0;
     for i in 0..14 {
         let w = chars.next().unwrap().to_digit(10).unwrap() as i64;
-        if z%26 + X_ADDERS[i] != w{
-            if Z_DIVISORS[i]{
-                z -= z%26;
-            }
-            else{
+        if z % 26 + X_ADDERS[i] != w {
+            if Z_DIVISORS[i] {
+                z -= z % 26;
+            } else {
                 z *= 26;
             }
             z += w + Y_ADDERS[i];
-        }
-        else if Z_DIVISORS[i]{
+        } else if Z_DIVISORS[i] {
             z /= 26;
         }
     }
@@ -81,9 +81,9 @@ fn _simulate_code(input: &str) -> i64 {
     let mut z = 0;
     for i in 0..14 {
         w = chars.next().unwrap().to_digit(10).unwrap() as i64;
-        x = z%26 + X_ADDERS[i];
+        x = z % 26 + X_ADDERS[i];
         z /= Z_DIVISORS[i];
-        if x != w{
+        if x != w {
             z *= 26;
             z += w + Y_ADDERS[i];
         }
@@ -94,16 +94,16 @@ fn _simulate_code(input: &str) -> i64 {
 fn _simulate_code_dbg(input: &str) -> i64 {
     const Z_DIVISORS: [i64; 14] = [1, 1, 1, 1, 26, 1, 1, 26, 1, 26, 26, 26, 26, 26];
     const X_ADDERS: [i64; 14] = [14, 13, 15, 13, -2, 10, 13, -15, 11, -9, -9, -7, -4, -6];
-    const Y_ADDERS: [i64; 14] = [ 0, 12, 14,  0,  3, 15, 11,  12,  1, 12,  3, 10, 14, 12];
+    const Y_ADDERS: [i64; 14] = [0, 12, 14, 0, 3, 15, 11, 12, 1, 12, 3, 10, 14, 12];
 
     println!("{}", input);
     let mut chars = input.chars();
     let mut z = 0;
     for i in 0..14 {
         let w = chars.next().unwrap().to_digit(10).unwrap() as i64;
-        let x = z%26 + X_ADDERS[i];
+        let x = z % 26 + X_ADDERS[i];
         z /= Z_DIVISORS[i];
-        if x != w{
+        if x != w {
             z *= 26;
             z += w + Y_ADDERS[i];
         }
@@ -112,14 +112,17 @@ fn _simulate_code_dbg(input: &str) -> i64 {
     z
 }
 
-fn _format_base_26(mut num: u64) -> String{
-    const CHAR_MAP: [char; 26] = ['0','1','2','3','4','5','6','7','8','9','A','B','C','D','E','F','G','H','I','J','K','L','M','N','O','P'];
+fn _format_base_26(mut num: u64) -> String {
+    const CHAR_MAP: [char; 26] = [
+        '0', '1', '2', '3', '4', '5', '6', '7', '8', '9', 'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H',
+        'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P',
+    ];
     let mut result = String::new();
-    if num == 0{
+    if num == 0 {
         result.push('0');
     }
-    while num > 0{
-        let digit = (num%26) as usize;
+    while num > 0 {
+        let digit = (num % 26) as usize;
         num /= 26;
         result.insert(0, CHAR_MAP[digit]);
     }
@@ -127,10 +130,10 @@ fn _format_base_26(mut num: u64) -> String{
 }
 
 #[cfg(test)]
-mod test{
+mod test {
     use super::*;
     #[test]
-    fn test_1(){
+    fn test_1() {
         let input = "11111111111111";
         let result = _simulate_code(input);
         println!("{}", result);
@@ -140,44 +143,45 @@ mod test{
     }
 
     #[test]
-    fn results_1(){
-        let results = [91131151917893_u64,
-        91131162917893_u64,
-        91131173917893_u64,
-        91131184917893_u64,
-        91131195917893_u64,
-        91131251918893_u64,
-        91131262918893_u64,
-        91131273918893_u64,
-        91131284918893_u64,
-        91131295918893_u64,
-        91131351919893_u64,
-        91131362919893_u64,
-        91131373919893_u64,
-        91131384919893_u64,
-        91131395919893_u64];
-        for i in results{
+    fn results_1() {
+        let results = [
+            91131151917893_u64,
+            91131162917893_u64,
+            91131173917893_u64,
+            91131184917893_u64,
+            91131195917893_u64,
+            91131251918893_u64,
+            91131262918893_u64,
+            91131273918893_u64,
+            91131284918893_u64,
+            91131295918893_u64,
+            91131351919893_u64,
+            91131362919893_u64,
+            91131373919893_u64,
+            91131384919893_u64,
+            91131395919893_u64,
+        ];
+        for i in results {
             let s = i.to_string();
             assert_eq!(_simulate_code(&s), 0);
         }
     }
 
     #[test]
-    fn simulation_dbg(){
+    fn simulation_dbg() {
         let input = "91131395919893";
         _simulate_code_dbg(input);
     }
 
     #[test]
-    fn my_answer(){
+    fn my_answer() {
         let input = "91297395919993";
         assert_eq!(_simulate_code_dbg(input), 0);
     }
 
     #[test]
-    fn my_answer_2(){
+    fn my_answer_2() {
         let input = "71131151917891";
         assert_eq!(_simulate_code_dbg(input), 0);
     }
-
 }
