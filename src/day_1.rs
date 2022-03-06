e4c01c8c1f1b1108cc3b8518dbdcbb0d59b3eb2c
smsutherland <sagan@farthinghalearms.com>
2022-03-05 20:55:37 -0500

    upgrade day 1 to use Vec::windows

--- src/day_1.rs
+++ src/day_1.rs
@@ -1,25 +1,16 @@
-use itertools::izip;
-
 pub fn run(lines: &[String]) -> (u64, u64) {
-    let depths = lines.iter().map(|x| x.parse::<u64>().unwrap());
+    let depths: Vec<u64> = lines.iter().map(|x| x.parse::<u64>().unwrap()).collect();
     let part_1 = depths
-        .clone()
-        .zip(depths.clone().skip(1))
-        .map(|x| x.0 < x.1)
-        .filter(|x| *x)
+        .windows(2)
+        .filter(|x| x[0] < x[1])
         .count() as u64;
 
-    let sums_of_3 = izip!(
-        depths.clone(),
-        depths.clone().skip(1),
-        depths.clone().skip(2)
-    )
-    .map(|x| x.0 + x.1 + x.2);
-    let part_2 = sums_of_3
-        .clone()
-        .zip(sums_of_3.clone().skip(1))
-        .map(|x| x.0 < x.1)
-        .filter(|x| *x)
+    let part_2 = 
+        depths.windows(3)
+        .map(|x| x[0] + x[1] + x[2])
+        .collect::<Vec<u64>>()
+        .windows(2)
+        .filter(|x| x[0] < x[1])
         .count() as u64;
 
     (part_1, part_2)
