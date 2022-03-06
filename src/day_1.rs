211cc9af04ed5450c0ca108feec31864ae8b1313
smsutherland <sagan@farthinghalearms.com>
2022-03-05 20:56:59 -0500

    fmt

--- src/day_1.rs
+++ src/day_1.rs
@@ -1,12 +1,9 @@
 pub fn run(lines: &[String]) -> (u64, u64) {
     let depths: Vec<u64> = lines.iter().map(|x| x.parse::<u64>().unwrap()).collect();
-    let part_1 = depths
-        .windows(2)
-        .filter(|x| x[0] < x[1])
-        .count() as u64;
+    let part_1 = depths.windows(2).filter(|x| x[0] < x[1]).count() as u64;
 
-    let part_2 = 
-        depths.windows(3)
+    let part_2 = depths
+        .windows(3)
         .map(|x| x[0] + x[1] + x[2])
         .collect::<Vec<u64>>()
         .windows(2)
